c2eab16d2c6410536ea4f56967169eecd79bcd58
bernad <bernadinusnaisau230803@gmail.com>
2025-09-23 21:41:10 +0700

    add main application class for Spring Boot setup

---
+++ src/main/java/contact/management/restfulapi/SpringContactManagementRestfulapiApplication.java
@@ -0,0 +1,13 @@
+package contact.management.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactManagementRestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactManagementRestfulapiApplication.class, args);
+	}
+
+}
