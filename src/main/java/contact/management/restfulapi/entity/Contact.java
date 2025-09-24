78aa4189ebb7a03d496c3b41715d3e5da21c3922
bernad <bernadinusnaisau230803@gmail.com>
2025-09-24 22:55:27 +0700

    add one-to-many relationship between Contact and Address entities (Contact Entity)

--- src/main/java/contact/management/restfulapi/entity/Contact.java
+++ src/main/java/contact/management/restfulapi/entity/Contact.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "contacts")
+@Entity
+@Table(name = "contacts")
 public class Contact {
     @Id
     private String id;
@@ -24,4 +27,7 @@ public class Contact {
     @ManyToOne
     @JoinColumn(name = "username", referencedColumnName = "username")
     private User user;
+
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
 }
