56e4cca9c39b9cf88e2bc3470e8cb326f96fdb5a
bernad <bernadinusnaisau230803@gmail.com>
2025-09-25 20:21:22 +0700

    add UserService for user registration with validation and password hashing (Register User Api)

---
+++ src/main/java/contact/management/restfulapi/service/UserService.java
@@ -0,0 +1,48 @@
+package contact.management.restfulapi.service;
+
+import contact.management.restfulapi.entity.User;
+import contact.management.restfulapi.model.RegisterUserRequest;
+import contact.management.restfulapi.repository.UserRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Validator validator;
+    @Transactional
+    public void register(RegisterUserRequest request) {
+        Set<ConstraintViolation<RegisterUserRequest>> violations = validator.validate(request);
+        if(violations.size() != 0){
+            //error
+            throw new ConstraintViolationException(violations);
+        }
+
+        //check if username already registered
+        if(userRepository.existsById(request.getUsername())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "username already registered");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
+        }
+    }
+
+
