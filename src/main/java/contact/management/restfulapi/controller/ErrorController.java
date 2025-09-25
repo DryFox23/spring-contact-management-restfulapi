94ed5f0a5ab481c17e66f845b817c48201379374
bernad <bernadinusnaisau230803@gmail.com>
2025-09-25 20:22:02 +0700

    add ErrorController for centralized exception handling with standardized API responses (Register User Api)

---
+++ src/main/java/contact/management/restfulapi/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package contact.management.restfulapi.controller;
+
+import contact.management.restfulapi.model.WebResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>> constraintViolationExeception(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(WebResponse.<String>builder().error(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<WebResponse<String>> apiException(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(WebResponse.<String>builder().error(exception.getReason()).build());
+    }
+}
