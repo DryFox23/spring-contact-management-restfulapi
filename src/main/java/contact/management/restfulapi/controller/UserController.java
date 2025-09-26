23c7edc5d5a5d132955daafaa8c92433f51b8576
bernad <bernadinusnaisau230803@gmail.com>
2025-09-26 23:28:52 +0700

    add endpoint to retrieve current user details (Get User Api)

--- src/main/java/contact/management/restfulapi/controller/UserController.java
+++ src/main/java/contact/management/restfulapi/controller/UserController.java
@@ -1,13 +1,14 @@
 package contact.management.restfulapi.controller;
 
+import contact.management.restfulapi.entity.User;
 import contact.management.restfulapi.model.RegisterUserRequest;
+import contact.management.restfulapi.model.UserResponse;
 import contact.management.restfulapi.model.WebResponse;
 import contact.management.restfulapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class UserController {
@@ -22,4 +23,13 @@ public class UserController {
         userService.register(registerUserRequest);
         return WebResponse.<String>builder().data("success").build();
     }
+
+    @GetMapping("/api/users/current")
+    public WebResponse<UserResponse> get(User user) {
+        UserResponse userResponse = userService.get(user);
+        return WebResponse.<UserResponse>builder()
+                .data(userResponse)
+                .build();
+    }
+
 }
