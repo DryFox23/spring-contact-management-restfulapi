c3ddfe53a627259d2aed1838c0c638449910ec67
bernad <bernadinusnaisau230803@gmail.com>
2025-09-28 21:21:21 +0700

    add ContactController for handling contact creation (Get Contact Api)

---
+++ src/main/java/contact/management/restfulapi/controller/ContactController.java
@@ -0,0 +1,33 @@
+package contact.management.restfulapi.controller;
+
+import contact.management.restfulapi.entity.User;
+import contact.management.restfulapi.model.ContactResponse;
+import contact.management.restfulapi.model.CreateContactRequest;
+import contact.management.restfulapi.model.WebResponse;
+import contact.management.restfulapi.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.codec.ServerSentEvent.builder;
+
+@RestController
+public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    // controller contact
+    @PostMapping(path = "/api/contacts",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request){
+        ContactResponse contactResponse = contactService.create(user, request);
+        return WebResponse.<ContactResponse>builder()
+                .data(contactResponse)
+                .build();
+    }
+}
