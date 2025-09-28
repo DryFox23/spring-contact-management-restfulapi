4cd5afca729cdd7dfcc43095f019469bf5fc6048
bernad <bernadinusnaisau230803@gmail.com>
2025-09-28 19:35:24 +0700

    add controller Delete for logout user (Logout User Api)

--- src/main/java/contact/management/restfulapi/controller/AuthController.java
+++ src/main/java/contact/management/restfulapi/controller/AuthController.java
@@ -1,15 +1,13 @@
 package contact.management.restfulapi.controller;
 
+import contact.management.restfulapi.entity.User;
 import contact.management.restfulapi.model.LoginUserRequest;
 import contact.management.restfulapi.model.TokenResponse;
 import contact.management.restfulapi.model.WebResponse;
 import contact.management.restfulapi.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
@@ -17,6 +15,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    // login controller
     @PostMapping(path = "/api/auth/login",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -25,5 +24,13 @@ public class AuthController {
         return WebResponse.<TokenResponse>builder()
                 .data(tokenResponse)
                 .build();
+    }
+
+    // logout controller
+    @DeleteMapping(path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<String> logout(User user){
+        authService.logout(user);
+        return WebResponse.<String>builder().data("success").build();
     }
 }
