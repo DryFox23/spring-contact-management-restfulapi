672ea577974ff90a09e8d31f06dd1de94dc3d392
bernad <bernadinusnaisau230803@gmail.com>
2025-09-28 21:22:40 +0700

    add ContactControllerTest for testing failed and sucess create contact

---
+++ src/test/java/contact/management/restfulapi/controller/ContactControllerTest.java
@@ -0,0 +1,104 @@
+package contact.management.restfulapi.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import contact.management.restfulapi.entity.User;
+import contact.management.restfulapi.model.ContactResponse;
+import contact.management.restfulapi.model.CreateContactRequest;
+import contact.management.restfulapi.model.WebResponse;
+import contact.management.restfulapi.repository.ContactRepository;
+import contact.management.restfulapi.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ContactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        // Menghapus semua data pada tabel contact dan user sebelum setiap pengujian
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+        // Menambahkan user untuk keperluan testing
+
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword(BCrypt.hashpw("test password", BCrypt.gensalt()));
+        user.setName("test name");
+        user.setToken("test token");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 1000000L);
+        userRepository.save(user);
+    }
+
+    @Test
+    void createContactBadRequest() throws Exception {
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("");
+        request.setEmail("salah email");
+
+        mockMvc.perform(post("/api/contacts")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test token")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isBadRequest()
+                ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+                    });
+                    assertNotNull(response.getError());
+                });
+    }
+
+    @Test
+    void createContactSuccess() throws Exception {
+        CreateContactRequest request = new CreateContactRequest();
+        request.setFirstName("Test First Name");
+        request.setLastName("Test Last Name");
+        request.setEmail("test@example.com");
+        request.setPhone("1234567890");
+
+        mockMvc.perform(post("/api/contacts")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-TOKEN", "test token")
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpectAll(
+                        status().isOk()
+                ).andDo(result -> {
+                    WebResponse<ContactResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<ContactResponse>>() {
+                    });
+                    assertNull(response.getError());
+                    assertNotNull(response.getData());
+                    assertEquals("Test First Name", response.getData().getFirstName());
+                    assertEquals("Test Last Name", response.getData().getLastName());
+                    assertEquals("test@example.com", response.getData().getEmail());
+                    assertEquals("1234567890", response.getData().getPhone());
+
+                    assertTrue(contactRepository.existsById(response.getData().getId()));
+                });
+    }
+}
