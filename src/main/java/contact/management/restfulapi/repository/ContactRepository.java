da444f903240f59c44a59828299c0c5d3b19501d
bernad <bernadinusnaisau230803@gmail.com>
2025-09-29 20:23:33 +0700

    add method findFirstByUsrAndId in ContactRepository (Get Contact Api)

--- src/main/java/contact/management/restfulapi/repository/ContactRepository.java
+++ src/main/java/contact/management/restfulapi/repository/ContactRepository.java
@@ -1,10 +1,14 @@
 package contact.management.restfulapi.repository;
 
 import contact.management.restfulapi.entity.Contact;
+import contact.management.restfulapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
 
+    Optional<Contact> findFirstByUserAndId(User user, String id);
 }
