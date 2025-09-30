291b9c7ab4411d7b1c028992aa6ecc25ff63dc5f
bernad <bernadinusnaisau230803@gmail.com>
2025-10-01 00:17:32 +0700

    add logic service for create address in AddressService(Create Address Api)

---
+++ src/main/java/contact/management/restfulapi/service/AddressService.java
@@ -0,0 +1,60 @@
+package contact.management.restfulapi.service;
+
+import contact.management.restfulapi.entity.Address;
+import contact.management.restfulapi.entity.Contact;
+import contact.management.restfulapi.entity.User;
+import contact.management.restfulapi.model.AddressResponse;
+import contact.management.restfulapi.model.CreateAddressRequest;
+import contact.management.restfulapi.repository.AddressRepository;
+import contact.management.restfulapi.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    ValidationService validationService;
+
+    @Transactional
+    public AddressResponse create(User user, CreateAddressRequest request){
+        validationService.validate(request);
+
+        Contact contact = contactRepository.findFirstByUserAndId(user, request.getContactId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found"));
+
+        Address address = new Address();
+        address.setId(UUID.randomUUID().toString());
+        address.setContact(contact);
+        address.setStreet(request.getStreet());
+        address.setCity(request.getCity());
+        address.setProvince(request.getProvince());
+        address.setCountry(request.getCountry());
+        address.setPostalCode(request.getPostalCode());
+        addressRepository.save(address);
+
+        return toAddressResponse(address);
+    }
+
+    private AddressResponse toAddressResponse(Address address) {
+        return AddressResponse.builder()
+                .id(address.getId())
+                .street(address.getStreet())
+                .city(address.getCity())
+                .province(address.getProvince())
+                .country(address.getCountry())
+                .postalCode(address.getPostalCode())
+                .build();
+    }
+}
