20bfbee52ad94f44e4fd1fe6fcef980b507afff7
bernad <bernadinusnaisau230803@gmail.com>
2025-09-30 19:47:42 +0700

    add unit test search contact like success and failed search contact(Search Contact Api)

--- src/test/java/contact/management/restfulapi/controller/ContactControllerTest.java
+++ src/test/java/contact/management/restfulapi/controller/ContactControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -166,7 +167,7 @@ public class ContactControllerTest {
 
         CreateContactRequest request = new CreateContactRequest();
         request.setFirstName("");
-        request.setEmail("test@example.com");
+        request.setEmail("salah");
 
         mockMvc.perform(put("/api/contacts/" + contact.getId())
                         .accept(MediaType.APPLICATION_JSON)
@@ -264,5 +265,113 @@ public class ContactControllerTest {
                     assertEquals("Delete contact success", response.getData());
                     assertFalse(contactRepository.existsById(contact.getId()));
                 });
+    }
+
+    // Search contact test case
+    @Test
+    void search404() throws Exception {
+
+        mockMvc.perform(get("/api/contacts")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-API-TOKEN", "test token"))
+        .andExpectAll(status().isOk())
+                .andDo(result -> {
+                    WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<List<ContactResponse>>>() {
+                    });
+                    assertNull(response.getError());
+                    assertEquals(0, response.getPaging().getTotalPage());
+                    assertEquals(0, response.getPaging().getCurrentPage());
+                    assertEquals(10, response.getPaging().getSize());
+                });
+    }
+
+    @Test
+    void searchContactSuccess() throws Exception {
+            User user = userRepository.findById("test username").orElseThrow();
+
+        for (int i = 0; i < 100; i++) {
+            Contact contact = new Contact();
+            contact.setId(UUID.randomUUID().toString());
+            contact.setUser(user);
+            contact.setFirstName("Test Username " + i);
+            contact.setLastName("Test Last Name " + i);
+            contact.setEmail("user" + i + "@example.com");
+            contact.setPhone("123456" + i);
+
+            contactRepository.save(contact);
+        }
+
+            mockMvc.perform(
+                    get("/api/contacts")
+                            .queryParam("name", "Test Username")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("X-API-TOKEN", "test token")
+            ).andExpectAll(
+                    status().isOk()
+            ).andDo(result -> {
+                WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                });
+                assertNull(response.getError());
+                assertEquals(10, response.getData().size());
+                assertEquals(10, response.getPaging().getTotalPage());
+                assertEquals(0, response.getPaging().getCurrentPage());
+                assertEquals(10, response.getPaging().getSize());
+            });
+
+            mockMvc.perform(
+                    get("/api/contacts")
+                            .queryParam("name", "Test Last Name")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("X-API-TOKEN", "test token")
+            ).andExpectAll(
+                    status().isOk()
+            ).andDo(result -> {
+                WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                });
+                assertNull(response.getError());
+                assertEquals(10, response.getData().size());
+                assertEquals(10, response.getPaging().getTotalPage());
+                assertEquals(0, response.getPaging().getCurrentPage());
+                assertEquals(10, response.getPaging().getSize());
+            });
+
+            mockMvc.perform(
+                    get("/api/contacts")
+                            .queryParam("email", "example.com")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("X-API-TOKEN", "test token")
+            ).andExpectAll(
+                    status().isOk()
+            ).andDo(result -> {
+                WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                });
+                assertNull(response.getError());
+                assertEquals(10, response.getData().size());
+                assertEquals(10, response.getPaging().getTotalPage());
+                assertEquals(0, response.getPaging().getCurrentPage());
+                assertEquals(10, response.getPaging().getSize());
+            });
+
+            mockMvc.perform(
+                    get("/api/contacts")
+                            .queryParam("phone", "3456")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("X-API-TOKEN", "test token")
+            ).andExpectAll(
+                    status().isOk()
+            ).andDo(result -> {
+                WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+                });
+                assertNull(response.getError());
+                assertEquals(10, response.getData().size());
+                assertEquals(10, response.getPaging().getTotalPage());
+                assertEquals(0, response.getPaging().getCurrentPage());
+                assertEquals(10, response.getPaging().getSize());
+            });
     }
 }
