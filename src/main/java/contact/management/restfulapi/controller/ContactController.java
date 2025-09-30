6a5a8731da4925063090746243a831f589b246ce
bernad <bernadinusnaisau230803@gmail.com>
2025-09-30 19:43:00 +0700

    add controller for search contact in ContactController (Search Contact Api)

--- src/main/java/contact/management/restfulapi/controller/ContactController.java
+++ src/main/java/contact/management/restfulapi/controller/ContactController.java
@@ -1,14 +1,14 @@
 package contact.management.restfulapi.controller;
 
 import contact.management.restfulapi.entity.User;
-import contact.management.restfulapi.model.ContactResponse;
-import contact.management.restfulapi.model.CreateContactRequest;
-import contact.management.restfulapi.model.UpdateContactRequest;
-import contact.management.restfulapi.model.WebResponse;
+import contact.management.restfulapi.model.*;
 import contact.management.restfulapi.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.codec.ServerSentEvent.builder;
 
@@ -60,6 +60,37 @@ public class ContactController {
         contactService.delete(user,contactId);
         return WebResponse.<String>builder()
                 .data("Delete contact success")
+                .build();
+    }
+
+    // Controller search contact
+//
+    @GetMapping(
+            path = "/api/contacts",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<ContactResponse>> search(User user,
+                                                     @RequestParam(value = "name", required = false) String name,
+                                                     @RequestParam(value = "email", required = false) String email,
+                                                     @RequestParam(value = "phone", required = false) String phone,
+                                                     @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+        SearchContactRequest request = SearchContactRequest.builder()
+                .page(page)
+                .size(size)
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .build();
+
+        Page<ContactResponse> contactResponses = contactService.search(user, request);
+        return WebResponse.<List<ContactResponse>>builder()
+                .data(contactResponses.getContent())
+                .paging(PagingResponse.builder()
+                        .currentPage(contactResponses.getNumber())
+                        .totalPage(contactResponses.getTotalPages())
+                        .size(contactResponses.getSize())
+                        .build())
                 .build();
     }
 }
