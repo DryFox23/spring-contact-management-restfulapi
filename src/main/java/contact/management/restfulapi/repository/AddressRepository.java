0dd3bab4ffb09b99c1867bfb2b8da52a7d7246f9
bernad <bernadinusnaisau230803@gmail.com>
2025-10-01 21:19:43 +0700

    add query method findFirsyByContactAndId for read query in AddressRepository (Get Address Api)

--- src/main/java/contact/management/restfulapi/repository/AddressRepository.java
+++ src/main/java/contact/management/restfulapi/repository/AddressRepository.java
@@ -1,10 +1,16 @@
 package contact.management.restfulapi.repository;
 
 import contact.management.restfulapi.entity.Address;
+import contact.management.restfulapi.entity.Contact;
+import contact.management.restfulapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
 
+    Optional<Address> findFirstByContactAndId(Contact contact, String id);
 }
